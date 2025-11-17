6e0df642ae6f43ac045e66beeaef161931dc782b
Shawn Kennedy <sdek@gmx.de>
2025-11-17 19:20:15 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// Platzhalter für Buchungssystem
+document.getElementById("openBooking").addEventListener("click", () => {
+    alert("Hier wird später das Buchungssystem eingebunden (cal.com).");
+});
